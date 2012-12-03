77ea18105db10bfdc7011a345c10db0cb29fb541
Rishi Goomar <rishig92@gmail.com>
2012-12-03 05:00:58 -0600

    Added scripts for conky file

---
+++ imap.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+# gimap.pl by gxmsgx
+# description: get the count of unread messages on imap
+
+use strict;
+use Mail::IMAPClient;
+use IO::Socket::SSL;
+
+my $username = 'rishig92@gmail.com'; 
+my $password = 'iruleu4life'; 
+
+my $socket = IO::Socket::SSL->new(
+  PeerAddr => 'imap.gmail.com',
+  PeerPort => 993
+ )
+ or die "socket(): $@";
+
+my $client = Mail::IMAPClient->new(
+  Socket   => $socket,
+  User     => $username,
+  Password => $password,
+ )
+ or die "new(): $@";
+
+if ($client->IsAuthenticated()) {
+   my $msgct;
+
+   $client->select("INBOX");
+   $msgct = $client->unseen_count||'0';
+   print "$msgct\n";
+}
+
+$client->logout();
